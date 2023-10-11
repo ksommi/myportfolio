cc7fbd37116f084fc1dd504de284762c04503ecb
ksommi <39311989+ksommi@users.noreply.github.com>
2023-10-11 20:41:13 -0300

    test 3

--- components/SkillCard/index.jsx
+++ components/SkillCard/index.jsx
@@ -1,19 +1,19 @@
 import Image from "next/image";
 
 const SkillCard = ({ title, img, invertColor }) => {
-  return (
-    <div className={`cardSingleContainer ${invertColor || ""}`}>
-      <Image
-        src={img}
-        width={60}
-        height={60}
-        className="cardSingleImage"
-        alt="Icono sobre la habilidad en cuestion"
-        objectFit="contain"
-      />
-      <h3 className="cardSingleTitle">{title}</h3>
-    </div>
-  );
+	return (
+		<div className={`cardSingleContainer ${invertColor || ""}`}>
+			<Image
+				src={img}
+				width={60}
+				height={60}
+				className="cardSingleImage"
+				alt="Icono sobre la habilidad en cuestion"
+				objectFit="contain"
+			/>
+			<h3 className="cardSingleTitle">{title}</h3>
+		</div>
+	);
 };
 
 export default SkillCard;
